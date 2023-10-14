8b36aaa0bf7ae4470819803c3e8eb5b063813441
hafeezrq <hafeezrq@yahoo.com>
2023-10-14 16:31:48 +1100

    Demo tuples in python.

---
+++ tuple.py
@@ -0,0 +1,13 @@
+# Demo tuples in Python
+
+# Create a tuple using constructor method
+mytuple = tuple(('Dave', 1, True))
+print(mytuple)
+
+# Create tuple
+atuple = (1, 5, "three", "seven")
+print(atuple)
+
+# Check type
+print(type(mytuple))
+print(type(atuple))
