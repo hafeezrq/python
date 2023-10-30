dfce2fcc4c2da0cf6d8ee51ce5f177446d968251
hafeezrq <hafeezrq@yahoo.com>
2023-10-30 16:51:35 +1100

    Refactor to use closure in python

--- rps.py
+++ rps.py
@@ -9,44 +9,56 @@ class RPS(Enum):
     SCISSORS = 3
 
 
-game_count = 0
-
-
 def rps():
+    game_count = 0
+    player_wins = 0
+    python_wins = 0
 
-    playerchoice = input(
-        "\nEnter...\n1 for Rock\n2 for Paper\n3 for Scissor:\n")
-    player = int(playerchoice)
+    def play_rps():
+        nonlocal player_wins
+        nonlocal python_wins
 
-    machinechoice = random.choice("123")
-    computer = int(machinechoice)
+        playerchoice = input(
+            "\nEnter...\n1 for Rock\n2 for Paper\n3 for Scissor:\n")
+        player = int(playerchoice)
 
-    if player < 1 or player > 3:
-        sys.exit("You must enter 1 or 2 or 3")
-    print("\nYou chose: " + str(RPS(player)).replace("RPS.", ""))
-    print("Computer chose: " + str(RPS(computer)).replace("RPS.", ""))
+        machinechoice = random.choice("123")
+        computer = int(machinechoice)
 
-    # Use nested function
-    def decide_winner(player, computer):
-        if (player == 1 and computer == 3) | (player == 2 and computer == 1) | (player == 3 and computer == 2):
-            return "🎉🎉🎉 You win! 🎉🎉🎉\n"
-        elif player == computer:
-            return "😲😲😲😲😲 Tie Game!😲😲😲😲😲\n"
-        else:
-            return "🎉🎉🎉  🐍 Python wins! 🎉🎉🎉\n"
+        if player < 1 or player > 3:
+            sys.exit("You must enter 1 or 2 or 3")
+        print("\nYou chose: " + str(RPS(player)).replace("RPS.", ""))
+        print("Computer chose: " + str(RPS(computer)).replace("RPS.", ""))
 
-    game_result = decide_winner(player, computer)
-    print(game_result)
+        # Use nested within nested function
+        def decide_winner(player, computer):
+            nonlocal player_wins
+            nonlocal python_wins
+            if (player == 1 and computer == 3) | (player == 2 and computer == 1) | (player == 3 and computer == 2):
+                player_wins += 1
+                return "\n🎉🎉🎉 You win! 🎉🎉🎉\n"
+            elif player == computer:
+                return "\n😲😲😲😲😲 Tie Game!😲😲😲😲😲\n"
+            else:
+                python_wins += 1
+                return "\n🎉🎉🎉  🐍 Python wins! 🎉🎉🎉\n"
 
-    # This is how you modify the global variable in python
-    global game_count
-    game_count += 1
-    # global variable modified above!
-    print("Game Count: " + str(game_count))
-    play_again = input("Play again?: Y for yes | N for no: ")
-    if play_again.lower() == "y":
-        rps()
-    sys.exit("Bye Bye!")
+        game_result = decide_winner(player, computer)
+        print(game_result)
+
+        nonlocal game_count
+        game_count += 1
+
+        print("Game Count: " + str(game_count))
+        print("Player wins: " + str(player_wins))
+        print("Computer wins: " + str(python_wins))
+        play_again = input("\nPlay again?: Y for yes | N for no: ")
+        if play_again.lower() == "y":
+            play_rps()
+        sys.exit("Bye Bye!")
+
+    return play_rps
 
 
-rps()
+play = rps()
+play()
