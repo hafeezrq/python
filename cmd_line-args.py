684452c54976e7c51f1f4b814230e52002377c38
hafeezrq <hafeezrq@yahoo.com>
2023-11-07 17:09:48 +1100

    Accept multipe agrs and use in function.

--- cmd_line-args.py
+++ cmd_line-args.py
@@ -1,15 +1,32 @@
 # Accepting and Using Command Line Arguments
 
-import argparse
+def greeting(name, language):
+    greetings_language = {
+        "English": "Hello",
+        "Spanish": "Hola",
+        "German": "Hallo"
+    }
+    message = f"{greetings_language[language]} {name}!"
+    print(message)
 
-parser = argparse.ArgumentParser(description="Provides a personal greetings")
+if __name__ == "__main__":
+    import argparse
 
-parser.add_argument(
-    "-n", "--name", 
-    metavar="name", 
-    required=True, 
-    help="Name of the person to greet."
-    )
-args = parser.parse_args()
-message = f"Hello {args.name}!"
-print(message)
+    parser = argparse.ArgumentParser(description="Provides a personal greetings")
+
+    parser.add_argument(
+        "-n", "--name", 
+        metavar="name", 
+        required=True, 
+        help="Name of the person to greet."
+        )
+    
+    parser.add_argument(
+        "-l", "--lang", 
+        metavar="language", 
+        required=True, 
+        help="The language of greeting."
+        )
+    
+    args = parser.parse_args()
+    greeting(args.name, args.lang)
